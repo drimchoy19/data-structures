c0cfee59e9e5b1a4e4fd2e90a3a6442bbf30af2a
Drim <drimchoy@gmail.com>
2018-11-14 23:34:50 +0200

    edit main

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -4,21 +4,21 @@ public class LinkedList<T> {
 	private int count;
 	private Node<T> head;
 
-	//removeByIndex,toArray
+	//toArray
 	public LinkedList() {
 
 		this.count = 0;
 		this.head = null;
 
 	}
-	
+
 	public LinkedList(T[] arr) {
 		LinkedList<T> ll = new LinkedList<T>();
-		
-		for(int i=0;i<arr.length;i++) {
-			
+
+		for (int i = 0; i < arr.length; i++) {
+
 			ll.add(arr[i]);
-			
+
 		}
 		this.head = ll.head;
 		this.count = ll.count;
@@ -33,7 +33,7 @@ public class LinkedList<T> {
 
 		} else {
 
-			Node <T> tmp = head;
+			Node<T> tmp = head;
 			while (tmp.getNext() != null) {
 
 				tmp = tmp.getNext();
@@ -67,59 +67,120 @@ public class LinkedList<T> {
 
 	}
 	
-	public T removeLast() {
+	public void show() {
 		
 		Node<T> tmp = head;
-		if(count==1) {
-			head = null;
-			return tmp.getData();
-		}else {
-		while(tmp.getNext().getNext()!=null) {
+		while(tmp!=null) {
 			
+			System.out.println(tmp.getData().toString());
 			tmp = tmp.getNext();
 			
 		}
-		Node<T> curr = tmp.getNext();
-		tmp.setNext(null);;
-		count--;
-		return curr.getData();
-	}
-	}
-	
-	public T getFirst() {
-		
-		return head.getData();
 		
 	}
-	
-	public T getLast() {
-		
-		Node<T> tmp = head;
-		while(tmp.getNext()!=null) {
+
+	public T remove(int index) {
+
+		Node<T> tmp = this.head;
+		Node<T> next = null;
+		Node<T> prev = null;
+
+		/*for(int j=0;j<index;j++) {
 			
+			System.out.println(""+j+" "+tmp);
 			tmp = tmp.getNext();
-			
+		}*/
+		if (index > count) {
+
+			return null;
+
+		} else if (index == 1) {
+			head = head.getNext();
+			count--;
+			return tmp.getData();
+
+		} else if (index == count) {
+
+			return this.removeLast();
+
+		} else {
+
+			for (int i = 0; i < index-1; i++) {
+				if (i == index-2) {
+					prev = tmp;
+					tmp = tmp.getNext();
+					next = tmp.getNext();
+					
+				}  else
+					tmp = tmp.getNext();
+
+			}
+			//System.out.println("tmp " + tmp);
+
+			//System.out.println("prev "+prev);
+			//System.out.println("next " + next);
+			prev.setNext(next);
+			count--;
+		}
+
+		return tmp.getData();
+
+	}
+
+	public T getFirst() {
+
+		return head.getData();
+
+	}
+
+	public T getLast() {
+
+		Node<T> tmp = head;
+		while (tmp.getNext() != null) {
+
+			tmp = tmp.getNext();
+
 		}
 		return tmp.getData();
 	}
-	
+
+	public T removeLast() {
+
+		Node<T> tmp = head;
+		if (count == 1) {
+			head = null;
+			return tmp.getData();
+		} else {
+			while (tmp.getNext().getNext() != null) {
+
+				tmp = tmp.getNext();
+
+			}
+			Node<T> curr = tmp.getNext();
+			tmp.setNext(null);
+			;
+			count--;
+			return curr.getData();
+		}
+	}
+
 	public T removeFirst() {
-		
+
 		T tmp = head.getData();
 		head = head.getNext();
 		count--;
 		return tmp;
-		
+
 	}
-	
+
 	public int size() {
 
 		return this.count;
 
 	}
-	
+
 	public boolean isEmpty() {
-		
-		return head == null ? true :  false;
+
+		return head == null ? true : false;
 	}
 }
--- src/Testing.java
+++ src/Testing.java
@@ -6,11 +6,10 @@ public class Testing {
 		
 
 		Person tmp = new Person();
-		Car tmpC = new Car();
 		Person[] p = tmp.initPersons();
-		Car[] cars = tmpC.initCars();
+		//Car tmpC = new Car();
+		//Car[] cars = tmpC.initCars();
 		
-		//System.out.println(tmp.getClass());
 		System.out.println("| 1 - LinkedList | 2 - Queue | 3 - Stack |");
 		int a = 0;
 		Scanner sc = new Scanner(System.in);
@@ -21,36 +20,49 @@ public class Testing {
 		
 		case 1 :
 			
-			System.out.println("LinkedList");
-			
 			LinkedList<Person> ll = new LinkedList<Person>();
-			LinkedList<Car> ll2 = new LinkedList<Car>(cars);
+			
 			for(int i=0;i<p.length;i++) {
 				
 				ll.add(p[i]);
 				in += p[i].getId()+" | ";//toString()+" ";
 				
 			}
+
+			System.out.println("LinkedListData");
+			ll.show();
+			System.out.println("isEmpty-"+ll.isEmpty());
+			System.out.println("LinkedList size-"+ll.size());
+			System.out.println("Remove 4 elements");
+			System.out.println(ll.remove(5).toString());
+			System.out.println(ll.remove(7).toString());
+			System.out.println(ll.remove(11).toString());
+			System.out.println(ll.remove(15).toString());
+			System.out.println("Size after remove-"+ll.size());
 			
-			System.out.println("IN");
-			System.out.println(in);
-			System.out.println("Size:"+ll.size());
+			ll.show();
+			
+			System.out.println("RemoveLast - "+ll.removeLast());
 			System.out.println("RemoveFirst - "+ll.removeFirst().toString());
+			System.out.println("Size:"+ll.size());
+			
+			//ll.show();
+
+			System.out.println("IN id's");
+			System.out.println(in);
 			while(ll.isEmpty()!=true) {
 				
-				tmp = ll.removeFirst();
-				out += tmp.getId()+" | ";//toString();
+				ll.removeFirst();
 				
 			}
-			
-			System.out.println(out);
+			System.out.println("isEmpty-"+ll.isEmpty());
 			
 			break;
 			
 		case 2 :
 			
-			System.out.println("Queue FIFO");
-			System.out.println("IN ORDER");
+			System.out.println("Queue FIFO(First In First Out)");
+			System.out.println("IN ORDER id's");
 			in = "";
 			for(int i = 0;i<p.length;) {
 				tmp = p[i];
@@ -61,8 +73,7 @@ public class Testing {
 			System.out.println(in);
 			Queue<Person> q = new Queue<Person>(p);
 			
-			//System.out.println(q.size());
-			System.out.println("OUT ORDER");
+			System.out.println("OUT ORDER id's");
 			 out = "";
 			while(q.isEmpty()!=true) {
 				
@@ -73,23 +84,23 @@ public class Testing {
 			}
 			
 			System.out.println(out);
-			//System.out.println(q.size());
 			
 			break;
 			
 		case 3 :
-			System.out.println("Stack LIFO");
+			System.out.println("Stack LIFO(Last In First Out");
 			
 			Stack<Person> st2 = new Stack<Person>(p);
 			in = "";
+			System.out.println("IN ORDER id's");
 			for(int i = 0;i<p.length;) {
 				tmp = p[i];
-				in += tmp.getId()+" | ";//+tmp.getName()+" ";
+				in += tmp.getId()+" | ";
 				i++;
 				
 			}
+			
 			System.out.println(in);
-			//System.out.println(st2.size());
 			out = "";
 			while(st2.isEmpty()!=true) {
 
@@ -98,17 +109,20 @@ public class Testing {
 				
 			
 			}
+			System.out.println("OUT ORDER id's");
 			System.out.println(out);
-			//System.out.println(st2.size());
+			
 			break;
 			
 			default :
 				
 				System.out.println("Wrong choice");
+				
 				break;
 		
 		}
 		
+		sc.close();
 		
 	}
 	
